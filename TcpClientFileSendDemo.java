6fa59131729b34608c39fab40477f73d3bf48f0f
INGUCoder <mazheng326326@gmail.com>
2020-05-19 18:04:57 +0800

    用tcp实现文件上传功能

--- TcpClientFileSendDemo.java
+++ TcpClientFileSendDemo.java
@@ -1,9 +1,6 @@
 package net;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -14,10 +11,13 @@ import java.net.UnknownHostException;
  */
 public class TcpClientFileSendDemo {
 
-    public static void main(String[] args){
-        Socket socket =null;
+    public static void main(String[] args) {
+        Socket socket = null;
         OutputStream outputStream = null;
         FileInputStream fileInputStream = null;
+        //创建管道输出流
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        InputStream inputStream = null;
 
         //创建连接地址
         try {
@@ -26,22 +26,47 @@ public class TcpClientFileSendDemo {
             //创建端口连接号
             int port = 9000;
             //创建socket连接
-             socket = new Socket(inetAddress,port);
+            socket = new Socket(inetAddress, port);
             //创建输出流
-             outputStream = socket.getOutputStream();
+            outputStream = socket.getOutputStream();
             //读取文件
-             fileInputStream = new FileInputStream(new File("C:\\Users\\mz\\Desktop\\Test\\src\\1.jpg"));
+            fileInputStream = new FileInputStream(new File("C:\\Users\\mz\\Desktop\\Test\\src\\1.jpg"));
             //写出文件 创建缓冲区
             byte[] bytes = new byte[1024];
             int len;
-            while ((len=fileInputStream.read(bytes))!=-1){
-                outputStream.write(bytes,0,len);
+            while ((len = fileInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, len);
             }
 
+            //通知服务器 已经传输完成
+            socket.shutdownOutput();
+            //确定服务器接收文件完毕才断开连接
+            inputStream = socket.getInputStream();
+
+            byte[] bytes2 = new byte[1024];
+            int len2;
+            while ((len2 = inputStream.read(bytes2)) != -1) {
+                byteArrayOutputStream.write(bytes2, 0, len2);
+            }
+            System.out.println(byteArrayOutputStream.toString());
 
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
+            if (byteArrayOutputStream != null) {
+                try {
+                    byteArrayOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             try {
                 outputStream.close();
             } catch (IOException e) {
@@ -59,7 +84,6 @@ public class TcpClientFileSendDemo {
             }
 
         }
-
 
 
     }
--- TcpServerFileGetDemo.java
+++ TcpServerFileGetDemo.java
@@ -1,9 +1,6 @@
 package net;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,53 +15,60 @@ public class TcpServerFileGetDemo {
         Socket socket = null;
         InputStream inputStream = null;
         FileOutputStream fileOutputStream = null;
-        try {
-            //创建服务端
-            serverSocket = new ServerSocket(9000);
-            //等待客户端响应
-            socket = serverSocket.accept();
-            //读取客户端流信息
-            inputStream = socket.getInputStream();
-            //输出文件
-            fileOutputStream = new FileOutputStream(new File("2.jpg"));
-            //创建缓存区
-            byte[] bytes = new byte[1024];
-            int len;
-            while ((len = inputStream.read(bytes)) != -1) {
-                fileOutputStream.write(bytes, 0, len);
+        while (true) {
+            try {
+                //创建服务端
+                serverSocket = new ServerSocket(9000);
+                //等待客户端响应
+                socket = serverSocket.accept();
+                //读取客户端流信息
+                inputStream = socket.getInputStream();
+                //输出文件
+                fileOutputStream = new FileOutputStream(new File("2.jpg"));
+                //创建缓存区
+                byte[] bytes = new byte[1024];
+                int len;
+                while ((len = inputStream.read(bytes)) != -1) {
+                    fileOutputStream.write(bytes, 0, len);
+                }
+                //通知客户端接收文件完毕
+                OutputStream outputStream = socket.getOutputStream();
+                outputStream.write("接收文件完毕！".getBytes());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fileOutputStream != null) {
+                    try {
+                        fileOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (serverSocket != null) {
+                    try {
+                        serverSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if(fileOutputStream!=null){
-                try {
-                    fileOutputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(inputStream!=null){
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(socket!=null){
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(serverSocket!=null){
-                try {
-                    serverSocket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+
         }
 
 
